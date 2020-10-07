7f03d0572f8f13e6a89712954b65ce8be3ec1cc8
mahmoud.ahmed <mahmoud.ahmed@benchmarklabs.io>
2020-10-07 18:25:15 +0200

    format files with flake8 linter

--- python/helper.py
+++ python/helper.py
@@ -11,12 +11,15 @@ def replace_chunk(content, marker, chunk):
     chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
     return replacer.sub(chunk, content)
 
+
 # Methods
 def ord(n):
-    return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
+    return str(n) + (
+        "th" if 4 <= n % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(n % 10, "th")
+    )
 
 
-def dtStylish(dt,f):
+def dtStylish(dt, f):
     return dt.strftime(f).replace("{th}", ord(dt.day))
 
 
@@ -26,7 +29,10 @@ def pprint(string):
 
 
 def compare_time(timestamp):
-    working_date = datetime.datetime.strftime(datetime.datetime.utcnow() - datetime.timedelta(minutes=5),"%Y-%m-%d %H:%M:%S") # 5 minutes ago
-    diff = datetime.datetime.strptime(working_date, "%Y-%m-%d %H:%M:%S") - datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+    working_date = datetime.datetime.strftime(
+        datetime.datetime.utcnow() - datetime.timedelta(minutes=5), "%Y-%m-%d %H:%M:%S"
+    )  # 5 minutes ago
+    diff = datetime.datetime.strptime(
+        working_date, "%Y-%m-%d %H:%M:%S"
+    ) - datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
     return int(round(int(diff.seconds) / 60, 0))
-
--- python/tocfcws.py
+++ python/tocfcws.py
@@ -1,4 +1,4 @@
- # importing modules
+# importing modules
 import os
 import json
 import pathlib
@@ -8,34 +8,40 @@ from requests import get
 import helper
 
 root = pathlib.Path(__file__).parent.parent.resolve()
-tweet_on = int(os.getenv('tweet'))
-tweet_window = int(os.getenv('window')) or 5
+tweet_on = int(os.getenv("tweet"))
+tweet_window = int(os.getenv("window")) or 5
 minute_offset = 65
 
-auth = tweepy.OAuthHandler(os.getenv('c_key'), os.getenv('c_secret'))
-auth.set_access_token(os.getenv('a_token'), os.getenv('a_secret'))
+auth = tweepy.OAuthHandler(os.getenv("c_key"), os.getenv("c_secret"))
+auth.set_access_token(os.getenv("a_token"), os.getenv("a_secret"))
 api = tweepy.API(auth)
 
+
 class article:
-        def __init__(self, title, url, timestamp):
-                self.title = title
-                self.url = url.replace('prod-content-cdn','www').replace('content/chelseafc/','').replace('.html','')
-                self.timestamp = timestamp
-        def __repr__(self):
-            return repr((self.title, self.url, self.timestamp))
+    def __init__(self, title, url, timestamp):
+        self.title = title
+        self.url = (
+            url.replace("prod-content-cdn", "www")
+            .replace("content/chelseafc/", "")
+            .replace(".html", "")
+        )
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return repr((self.title, self.url, self.timestamp))
 
 
 def get_news(num):
-    endpoint = ( os.getenv('json_url') )
+    endpoint = os.getenv("json_url")
     response = get(endpoint, timeout=10)
     articles = list()
     if response.status_code >= 400:
-        raise RuntimeError(f'Request failed: { response.text }')
-    data = response.json()['newsFeed'][:num]
+        raise RuntimeError(f"Request failed: { response.text }")
+    data = response.json()["newsFeed"][:num]
     for i in range(0, num):
-        link = data[i]['link']['url']
-        title = data[i]['link']['title']
-        time = data[i]['publishDate']
+        link = data[i]["link"]["url"]
+        title = data[i]["link"]["title"]
+        time = data[i]["publishDate"]
         try:
             published_matches = list(datefinder.find_dates(time))
             if len(published_matches) > 0:
@@ -48,20 +54,22 @@ def get_news(num):
         articles.append(article(title, link, published_str_dt))
     return articles
 
+
 # output
 if __name__ == "__main__":
     index_page = root / "index.html"
     index_contents = index_page.open().read()
-    string_output = ''
+    string_output = ""
     for article in get_news(8):
         output_builder = f"{article.title} :- {article.url}"
         print("★", article.title)
         string_output += f'<li><a href="{article.url}">{article.title}</a><br/><small>{article.timestamp}</small></li>\n'
-        if(compare_time(article.timestamp) < int(tweet_window)):
-            if(tweet_on == 1):
+        if compare_time(article.timestamp) < int(tweet_window):
+            if tweet_on == 1:
                 tweet_string = f"★ {output_builder}"
                 print("tweet length: ", len(tweet_string))
-                api.update_status(status = tweet_string)
-    final_output = helper.replace_chunk(index_contents, "news_marker", f"<ul>\n{string_output}</ul>")
+                api.update_status(status=tweet_string)
+    final_output = helper.replace_chunk(
+        index_contents, "news_marker", f"<ul>\n{string_output}</ul>"
+    )
     index_page.open("w").write(final_output)
-
