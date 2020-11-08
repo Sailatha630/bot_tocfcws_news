9ff6775b13987faa4826d24d88bad1453bb63f7f
MatBenfield <MatBenfield@users.noreply.github.com>
2020-11-08 16:20:35 +0000

    url -> link
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- python/outputs.py
+++ python/outputs.py
@@ -38,7 +38,7 @@ def rss_output(path, articles_list):
                  <description>{article.title}</description>
                  <pubDate>{article.timestamp}</pubDate>
                  <guid isPermaLink="false">{article.id}</guid>
-                 <url>{article.url}</url>
+                 <link>{article.url}</link>
                 </item>
             ''').strip(), "  "), file=rssfile)
 
